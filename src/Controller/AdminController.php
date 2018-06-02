ce259d1c4117f71f13b8bb7310996fcecd9cad18
Umar Enesi Ibrahim <contactenesi@gmail.com>
2018-06-02 08:57:08 +0100

    no message for now

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,26 +5,58 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class AdminController extends Controller
-{
+class AdminController extends Controller {
+
     /**
      * @Route("/kssbadmin", name="admin")
      */
-    public function index(Request $request)
-    {
+    public function index(Request $request) {
         $rep = $this->getDoctrine()->getRepository(\App\Entity\User::class);
         $userstat = array();
-        $userstat['registered']= $rep->countAllApplicants();
-        $userstat['paid']= $rep->countAllApplicants('paid');
-        $userstat['completed']= $rep->countAllApplicants('completed');
-        
+        $userstat['registered'] = $rep->countAllApplicants();
+        $userstat['paid'] = $rep->countAllApplicants('paid');
+        $userstat['completed'] = $rep->countAllApplicants('completed');
+
         return $this->render('admin/index.html.twig', [
-            'page'=>'index',
-            'contentTitle'=>'Dashboard',
-            'titleIcon'=>'<i class="fa fa-dashboard fa-fw"></i>',
-            'controller_name' => 'AdminController',
-            'userstat'=>$userstat
+                    'page' => 'index',
+                    'contentTitle' => 'Dashboard',
+                    'titleIcon' => '<i class="fa fa-dashboard fa-fw"></i>',
+                    'controller_name' => 'AdminController',
+                    'userstat' => $userstat
         ]);
     }
+
+    /**
+     * @Route("/kssbadmin/countregistered", name="countregisteredajax")
+     */
+    public function getCountRegistered(Request $request) {
+        if ($request->isXmlHttpRequest()) {
+            $rep = $this->getDoctrine()->getRepository(\App\Entity\User::class);
+            $registered = $rep->countAllApplicants();
+            return new Response($registered);
+        }
+    }
+    /**
+     * @Route("/kssbadmin/countpaid", name="countpaidajax")
+     */
+    public function getCountPaid(Request $request) {
+        if ($request->isXmlHttpRequest()) {
+            $rep = $this->getDoctrine()->getRepository(\App\Entity\User::class);
+            $paid = $rep->countAllApplicants('paid');
+            return new Response($paid);
+        }
+    }
+    /**
+     * @Route("/kssbadmin/countcompleted", name="countcompletedajax")
+     */
+    public function getCountCompleted(Request $request) {
+        if ($request->isXmlHttpRequest()) {
+            $rep = $this->getDoctrine()->getRepository(\App\Entity\User::class);
+            $completed = $rep->countAllApplicants('completed');
+            return new Response($completed);
+        }
+    }
+
 }
